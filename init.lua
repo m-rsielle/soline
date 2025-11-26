eaf08a1a6a13bb58da1429063b0f1e075636ed6b
ricoo <marvx35@gmail.com>
2025-11-26 09:19:39 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,22 @@
 local root = "https://raw.githubusercontent.com/m-rsielle/soline/main" 
 local loadt = os.date("%Y-%m-%d %H:%M:%S", os.time())
-local file = {
-    "/src/esp.lua",
-    "/src/main.lua",
-}
 
-for _, dir in ipairs(file) do
-    local success, err = pcall(function()
-        local fn = loadstring(game:HttpGet(root .. dir))
-        if not fn then error("Failed to load: " .. dir) end
-        fn()
-    end)
-    if not success then
-        warn("Error loading " .. dir .. ": " .. tostring(err))
-    end
+local success, err = pcall(function()
+    local fn = loadstring(game:HttpGet(root .. "/src/esp.lua"))
+    if not fn then error("Failed to load esp.lua") end
+    getgenv().esp = fn()
+end)
+if not success then
+    warn("Error loading esp.lua: " .. tostring(err))
+end
+
+success, err = pcall(function()
+    local fn = loadstring(game:HttpGet(root .. "/src/main.lua"))
+    if not fn then error("Failed to load main.lua") end
+    fn()
+end)
+if not success then
+    warn("Error loading main.lua: " .. tostring(err))
 end
 
 print("Loaded in " .. loadt)
